fcd9c204fae6e35bdc4c20b2a5519872ad9813ef
amerhajali2000 <87277407+amerhajali2000@users.noreply.github.com>
2022-03-13 17:22:34 +0300

    Update dataconfig.js

--- config/dataconfig.js
+++ config/dataconfig.js
@@ -1,4 +1,4 @@
 module.exports={
-    db:'mongodb+srv://amerhajali:amer732393@cluster0.p2spq.mongodb.net/live_coaching?retryWrites=true&w=majority',
+    db:'mongodb+srv://********:********@cluster0.p2spq.mongodb.net/live_coaching?retryWrites=true&w=majority',
     secret:'livec2022'
 }
